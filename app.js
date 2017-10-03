05f870c6a09aa92bcd808af4288481353a152a41
jai106 <jaib610@gmail.com>
2017-10-03 14:32:56 +0530

    angular

---
+++ app.js
@@ -0,0 +1,103 @@
+var myApp = angular.module('myApp', ['ngRoute'])
+
+//ng-route config
+.config(function ($routeProvider, $locationProvider){
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'default.html',
+    })
+    .when('/contact-info/:contact_index', {
+      templateUrl: 'contact_info.html',
+      controller: 'contactInfoCtrl'
+    })
+    .when('/add', {
+      templateUrl: 'contact_form.html',
+      controller: 'addContactCtrl'
+    })
+    .when('/edit/:contact_index', {
+      templateUrl: 'contact_form.html',
+      controller: 'editContactCtrl'
+    })
+    .otherwise({redirectTo: '/home'});
+})
+
+// controllers
+.controller('navCtrl', function ($scope) {
+  $scope.nav = {
+    navItems: ['home', 'add'],
+    selectedIndex: 0,
+    navClick: function ($index) {
+      $scope.nav.selectedIndex = $index;
+    }
+  };
+})
+
+.controller('homeCtrl', function ($scope, ContactService){
+  $scope.contacts = ContactService.getContacts();
+
+  $scope.removeContact = function (item) {
+    var index = $scope.contacts.indexOf(item);
+    $scope.contacts.splice(index, 1);
+    $scope.removed = 'Contact successfully removed.';
+  };
+
+})
+
+.controller('contactInfoCtrl', function ($scope, $routeParams){
+  var index = $routeParams.contact_index;
+  $scope.currentContact = $scope.contacts[index];
+})
+
+.controller('addContactCtrl', function ($scope, $location) {
+  //needed to show the correct button on the contact form
+  $scope.path = $location.path();
+
+  $scope.addContact = function () {
+    var contact = $scope.currentContact;
+    contact.id = $scope.contacts.length;
+    $scope.contacts.push(contact);
+  };
+
+})
+
+.controller('editContactCtrl', function ($scope, $routeParams){
+  $scope.index = $routeParams.contact_index;
+  $scope.currentContact = $scope.contacts[$scope.index];
+})
+
+// directives
+.directive('contact', function () {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'contact.html'
+  }
+})
+
+// services
+.factory('ContactService', [function () {
+  var factory = {};
+
+  factory.getContacts = function () {
+    return contactList;
+  }
+
+  // contact list, usually would be a separate database
+  var contactList = [
+    {id: 0, name: 'Ned Stark', email: 'ned@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Winter is coming.'},
+    {id: 1, name: 'Theon Greyjoy', email: 'tgreyjoy@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Reluctant to pay iron price.'},
+    {id: 2, name: 'Samwell Tarly', email: 'starly@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Loyal brother of the watch.'},
+    {id: 3, name: 'Jon Snow', email: 'jsnow@castleblack.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Knows nothing.'},
+    {id: 4, name: 'Arya Stark', email: 'waterdancer@winterfell.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Has a list of names.'},
+    {id: 5, name: 'Jora Mormont', email: 'khaleesifan100@gmail.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Lost in the friend-zone.'},
+    {id: 6, name: 'Tyrion Lannister', email: 'tyrion@lannister.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Currently drunk.'},
+    {id: 7, name: 'Stannis Baratheon', email: 'onetrueking@dragonstone.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Nobody expects the Stannish inquisition.'},
+    {id: 8, name: 'Hodor', email: 'hodor@hodor.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Hodor? Hodor... Hodor!'},
+    {id: 9, name: 'Margaery Tyrell', email: 'mtyrell@highgarden.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Keeper of kings.'},
+    {id: 10, name: 'Brienne of Tarth', email: 'oathkeeper@gmail.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not cross her.'},
+    {id: 11, name: 'Petyr Baelish', email: 'petyr@baelishindustries.com', phone: '123-456-7890', url: 'www.google.com', notes: 'Do not trust anyone.'},
+  ];
+  
+  return factory;
+}]);
+
